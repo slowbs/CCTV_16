bab27e03788a9db68ed5a1e3afebfe4139cda1f6
slowbs <poison_word@hotmail.com>
2024-04-17 16:34:52 +0700

    call Function Error
    ยังคง Error อยู่เมื่อมีการเรียกใช้ Function จาก main.js

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BsDropdownConfig, BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { AppRouting } from './app.routing';
